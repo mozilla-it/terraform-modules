9b7291243df5c67f0024e3d1969842b56f1ca3f7
Alberto del Barrio <adelbarrio@mozilla.com>
2020-02-14 12:25:29 +0100

    Terraform 0.12 conditional

--- vpc/subnets/main.tf
+++ vpc/subnets/main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "subnets" {
-  count = var.enabled * length(var.azs)
+  count = (var.enabled == "false" ? 0 : length(var.azs))
 
   triggers = {
     public_subnets = cidrsubnet(var.vpc_cidr, var.newbits, count.index % length(var.azs))
