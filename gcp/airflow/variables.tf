25edfa3577039c7432e123bc2fadfb8a386a89bd
Adam Frank <afrank@mozilla.com>
2020-03-05 08:40:00 -0800

    adding tf12-converted airflow module

---
+++ gcp/airflow/variables.tf
@@ -0,0 +1,45 @@
+variable "admin_project" {
+}
+
+variable "project" {
+}
+
+variable "environment" {
+}
+
+variable "region" {
+}
+
+variable "sendgrid_mail_from" {
+}
+
+variable "sendgrid_api_key" {
+}
+
+variable "network" {
+}
+
+variable "node_count_default" {
+}
+
+variable "disk_size_gb_default" {
+}
+
+variable "machine_type_default" {
+}
+
+variable "node_count_additional" {
+}
+
+variable "disk_size_gb_additional" {
+}
+
+variable "machine_type_additional" {
+}
+
+variable "cost_center" {
+}
+
+variable "project_code" {
+}
+
