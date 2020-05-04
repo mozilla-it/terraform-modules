38b7b8a8b5838ec112f2b217a40bedb4960b60eb
limed <limed@mozilla.com>
2020-05-04 10:19:27 -0700

    Rename velero bucket to take into account clusters in different regions

--- aws/velero-bucket/locals.tf
+++ aws/velero-bucket/locals.tf
@@ -1,6 +1,10 @@
 locals {
-  bucket_name = "${var.bucket_name}-${var.cluster_name}-${data.aws_caller_identity.current.account_id}"
-  backup_user = "${var.backup_user}-${var.cluster_name}"
+
+  bucket_name_default = "${var.bucket_name}-${var.cluster_name}-${var.region}-${data.aws_caller_identity.current.account_id}"
+  backup_user_default = "${var.backup_user}-${var.cluster_name}-${var.region}"
+
+  bucket_name = merge(local.bucket_name_default, var.bucket_name)
+  backup_user = merge(local.backup_user_default, var.backup_user)
 
   tags = merge(
     {
