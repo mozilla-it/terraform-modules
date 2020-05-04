b062349f2b89a7450fd1a8c73bce561e460758fe
limed <limed@mozilla.com>
2020-05-04 13:48:10 -0700

    Maintain default naming behaviour to minimize other peoples pain

--- aws/velero-bucket/locals.tf
+++ aws/velero-bucket/locals.tf
@@ -1,7 +1,7 @@
 locals {
 
-  bucket_name_default = "${var.bucket_name}-${var.cluster_name}-${var.region}-${data.aws_caller_identity.current.account_id}"
-  backup_user_default = "${var.backup_user}-${var.cluster_name}-${var.region}"
+  bucket_name_default = "velero-${var.cluster_name}-${data.aws_caller_identity.current.account_id}"
+  backup_user_default = "velero-${var.cluster_name}"
 
   bucket_name = merge(local.bucket_name_default, var.bucket_name)
   backup_user = merge(local.backup_user_default, var.backup_user)
