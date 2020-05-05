318c56a75432d6bf8611829a71548e6a78d249a6
limed <limed@mozilla.com>
2020-05-04 17:42:53 -0700

    Update velero role to take into account region

--- aws/velero-bucket/locals.tf
+++ aws/velero-bucket/locals.tf
@@ -1,7 +1,7 @@
 locals {
 
   bucket_name   = var.bucket_name == "" ? "velero-${var.cluster_name}-${data.aws_caller_identity.current.account_id}" : var.bucket_name
-  backup_user   = var.backup_user == "" ? "velero-${var.cluster_name}" : var.backup_user
+  backup_user   = var.backup_user == "" ? "velero-${var.cluster_name}-${var.region}" : var.backup_user
   kms_key_alias = var.kms_key_alias == "" ? "velero-${var.cluster_name}-${var.region}" : var.kms_key_alias
 
   tags = merge(
