0376525ba7cf497dfb191adad0156519c24ecdab
Chris Valaas <cvalaas@mozilla.com>
2020-05-26 09:54:06 -0700

    don't like four spaces?

--- gcp/airflow/variables.tf
+++ gcp/airflow/variables.tf
@@ -1,6 +1,6 @@
 variable "composer_image_version" {
-    type = string
-    default = "composer-1.8.1-airflow-1.10.3"
+  type = string
+  default = "composer-1.8.1-airflow-1.10.3"
 }
 
 variable "admin_project" {
