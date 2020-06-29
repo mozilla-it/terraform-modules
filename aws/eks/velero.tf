aa71ac7d0d636e5819efeaf19ccba4faca8b3c11
limed <limed@mozilla.com>
2020-06-29 14:33:10 -0700

    Fix velero role creation error when velero is set to false in cluster_features

--- aws/eks/velero.tf
+++ aws/eks/velero.tf
@@ -14,6 +14,7 @@ module "velero" {
   region              = var.region
   cluster_name        = module.eks.cluster_id
   create_bucket       = var.create_eks && local.cluster_features["velero"]
+  create_role         = var.create_eks && local.cluster_features["velero"]
   bucket_name         = var.velero_bucket_name
   velero_sa_namespace = "velero"
   velero_sa_name      = "velero"
