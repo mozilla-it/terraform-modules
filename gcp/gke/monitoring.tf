df7631a928a2c7b62eb74c677ed5fc1af9965c46
limed <limed@mozilla.com>
2020-08-03 11:34:56 -0700

    Configure prometheus to use ssd

--- gcp/gke/monitoring.tf
+++ gcp/gke/monitoring.tf
@@ -2,5 +2,6 @@ module "prometheus" {
   source                   = "github.com/mozilla-it/terraform-modules//helm/prometheus?ref=master"
   enabled                  = local.cluster_features["prometheus"]
   cloud_provider           = "gcp"
+  storage_class            = { "gcp" = "faster" }
   prometheus_helm_settings = var.prometheus_settings
 }
