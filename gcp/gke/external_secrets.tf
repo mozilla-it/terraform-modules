02f1124211bbc450caccb36935af6ae928d44290
limed <limed@mozilla.com>
2020-08-05 14:44:08 -0700

    Fix external secrets to support multiple gsa

--- gcp/gke/external_secrets.tf
+++ gcp/gke/external_secrets.tf
@@ -3,7 +3,8 @@ module "external-secrets-workload-identity" {
   source      = "github.com/mozilla-it/terraform-modules//gcp/identity?ref=master"
   enabled     = local.cluster_features["external_secrets"]
   create_ksa  = false # Allow chart to create
-  name        = "kubernetes-external-secrets"
+  name        = "kubernetes-external-secrets-${module.gke.name}"
+  ksa_name    = "kubernetes-external-secrets" # If this value changes make sure local.tf value changes too
   namespace   = "kube-system"
   gke_cluster = module.gke.name
   project_id  = var.project_id
