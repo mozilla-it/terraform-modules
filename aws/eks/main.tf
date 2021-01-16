715dc88689e21428e19da7bef7604d1678b01c8d
limed <limed@mozilla.com>
2021-01-15 16:21:08 -0800

    Bump iam module version

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -4,12 +4,9 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1"
 }
 
 provider "helm" {
-  version = "~> 1"
-
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
