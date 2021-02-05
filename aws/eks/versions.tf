2a82b2e76f2e95d5cf52a3984080aefe9a5a0c1c
limed <limed@mozilla.com>
2021-02-05 08:45:32 -0800

    Pin version for aws, kubernetes and helm providers

--- aws/eks/versions.tf
+++ aws/eks/versions.tf
@@ -1,3 +1,18 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~>2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~>2"
+    }
+  }
 }
