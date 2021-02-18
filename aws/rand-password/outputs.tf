5d7682c824f605b1c9a115695e6b7ac87fb101c6
limed <limed@mozilla.com>
2021-02-17 16:48:26 -0800

    updating module to input password path as an option

--- aws/rand-password/outputs.tf
+++ aws/rand-password/outputs.tf
@@ -2,6 +2,10 @@ output "password_store" {
   value = var.password_store
 }
 
+output "password_path" {
+  value = local.password_path
+}
+
 output "password" {
   sensitive = true
   value     = concat(random_password.password.*.result, [""])[0]
