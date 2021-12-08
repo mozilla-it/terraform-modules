5daac480eb8dce1b7826a94c7210db66df555160
Rogerio Varalda <rvaralda@mozilla.com>
2021-12-08 13:36:41 +0100

    clean up test

--- aws/eks/velero.tf
+++ aws/eks/velero.tf
@@ -14,7 +14,7 @@ resource "kubernetes_namespace" "velero" {
 }
 
 module "velero" {
-  source              = "github.com/mozilla-it/terraform-modules//aws/velero-bucket?ref=update-aws-velero-module-output"
+  source              = "github.com/mozilla-it/terraform-modules//aws/velero-bucket?ref=master"
   region              = var.region
   cluster_name        = module.eks.cluster_id
   create_bucket       = var.create_eks && local.cluster_features["velero"]
