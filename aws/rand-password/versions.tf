e1b9ead7c2997658a6dbe6961b31929102c50c67
Brett Kochendorfer <bkochendorfer@mozilla.com>
2022-05-18 14:41:08 -0500

    Testing unpinning AWS 3

--- aws/rand-password/versions.tf
+++ aws/rand-password/versions.tf
@@ -1,16 +1,15 @@
-
 terraform {
   required_version = ">= 0.13"
   required_providers {
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3"
+      version = "> 3"
     }
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3"
+      version = "> 3"
     }
   }
 }
